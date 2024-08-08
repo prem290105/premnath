41413fe80a467ede5f8d83a1d23d389ad8c15680
prem290105 <prem290105@gmail.com>
2024-08-08 09:27:18 +0530

    Add files via upload

---
+++ styles/app.js
@@ -0,0 +1,21 @@
+//let grade = 87;
+
+//if (grade >= 90) {
+  //  console.log("A");
+//} else if (grade >= 80) {
+  //  console.log("B");
+//} else if (grade >= 70) {
+  //  console.log("C");
+//} else if (grade >= 60) {
+  //  console.log("D");
+//} else {
+  //  console.log("F");
+//}
+
+
+function myfunc()
+{
+    let a=parseInt(document.getElementsById(num1).valueOf())
+    let b=parseInt(document.geyElementsById(num2).valueOf())
+    
+}
